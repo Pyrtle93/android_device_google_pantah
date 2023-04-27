618fbb693c4776dfe2e5e31e8f03d075a102e317
Matt Filetto <matt.filetto@gmail.com>
2023-04-27 11:04:07 -0700

    Add aggregated sysconfig & permissions XMLs with items from stock OS
    * Keep these all in one place for easier managment.
    * include all the pixel experiences
    
    Change-Id: I29e21970a0f39153efa72b3e633f87f05d86b95b
    
    # Conflicts:
    #	device-lineage.mk
    
    # Conflicts:
    #	device-banana.mk
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -20,6 +20,11 @@ include hardware/google/pixel/powershare/device.mk
 # Face unlock
 include vendor/google/faceunlock/device.mk
 
+# sysconfig and permissions XML from stock
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml \
+    $(LOCAL_PATH)/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+
 # Build necessary packages for vendor
 
 # Audio
