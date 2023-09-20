42593b2a7685adbf50f7bfbf6b6866915a3c4d07
Pyrtle93 <xxpyrtlexx93@gmail.com>
2023-09-20 17:30:32 -0400

    pantah: prepare for AlphaDroid

--- lineage_panther.mk
+++ lineage_panther.mk
@@ -6,13 +6,49 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
 $(call inherit-product, device/google/gs201/lineage_common.mk)
 
 include device/google/pantah/panther/device-lineage.mk
+
+# AlphaDroid Xtras
+WITH_GAPPS := true
+TARGET_HAS_UDFPS := true
+TARGET_INCLUDE_PIXEL_FRAMEWORK := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_USES_GOOGLE_DIALER_CARRIER_SETTINGS :=true
+TARGET_SUPPORTS_QUICK_TAP := true
+# Included by default
+TARGET_INCLUDE_GOOGLE_SETUP := true
+TARGET_INCLUDE_GOOGLE_FILE_MANAGER := true
+TARGET_INCLUDE_GOOGLE_BATTERY_STATS := true
+TARGET_INCLUDE_GOOGLE_CONTACTS := true
+TARGET_INCLUDE_GOOGLE_MESSAGING := true
+TARGET_INCLUDE_GOOGLE_CALENDAR := true
+TARGET_INCLUDE_GBOARD := true
+TARGET_INCLUDE_GOOGLE_APP := true
+TARGET_INCLUDE_GOOGLE_PHOTOS := true
+TARGET_INCLUDE_ANDROID_AUTO := true
+TARGET_INCLUDE_GOOGLE_CALCULATOR := true
+TARGET_INCLUDE_GOOGLE_CHROME := true
+TARGET_INCLUDE_GOOGLE_CLOCK := true
+TARGET_INCLUDE_GOOGLE_MARKUP := true
+TARGET_INCLUDE_GOOGLE_SOUNDS := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := true
+TARGET_DEFAULT_ADB_ENABLED := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Pyrtle93
+
+# Parts
+$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
